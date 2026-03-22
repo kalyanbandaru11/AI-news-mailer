96ab14e5231d9c325d8b8350d04c07dffc8c0eab
kalyanbandaru11 <kalyanbandaru11@gmail.com>
2026-03-22 23:43:38 +0530

    Refactor ProjectAI.py for cleaner code and email sending

--- ProjectAI.py
+++ ProjectAI.py
@@ -1,28 +1,16 @@
 import requests
-# from openai import OpenAI
 import smtplib
 from email.mime.text import MIMEText
-import schedule
-import time
-from datetime import datetime, timedelta
 import os
 
-NEWS_API_KEY = os.getenv("a92fa6fc0de9443f94f7f6f43534545b")
-SENDER_EMAIL = os.getenv("kalyanbandaru11@gmail.com")
-APP_PASSWORD = os.getenv("elzh wfbd ztsc hnfp")
-RECEIVER_EMAIL = os.getenv("bandarukalyan1107@gmail.com")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
 
 # ===================== CONFIG =====================
-
-
-
 SEND_TIME = "09:00"   # daily time
-
 # ==================================================
-
-#client = OpenAI(api_key=OPENAI_API_KEY)
-
-
 def get_news():
     url = (
         "https://newsapi.org/v2/everything?"
@@ -55,20 +43,7 @@ def get_news():
 
     return news_list
 
-def summarize_news(news_list):
-    news_text = "\n".join(news_list)
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {
-                "role": "user",
-                "content": f"Give me 5 clear bullet points of latest AI news:\n{news_text}"
-            }
-        ]
-    )
-
-    return response.choices[0].message.content
 
 
 def send_email(content):
@@ -77,12 +52,10 @@ def send_email(content):
     msg["From"] = SENDER_EMAIL
     msg["To"] = RECEIVER_EMAIL
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     server.login(SENDER_EMAIL, APP_PASSWORD)
     server.send_message(msg)
     server.quit()
-
 
 def format_news(news_list):
     content = "🧠 Daily AI News (Last 24 Hours)\n\n"
